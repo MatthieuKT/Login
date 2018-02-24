9882357f881ef05a3244b56a0b4e4006db5ff4fd
kwant <kwant.matthieu@gmail.com>
2018-02-24 21:07:41 +0100

    création la page login et son controlleur

--- Controller/SignControl.php
+++ Controller/SignControl.php
@@ -1,32 +1,34 @@
 <?php
 
 if (isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['passControl']) && $_POST['pass'] === $_POST['passControl'] ) {
-
 	$email = htmlspecialchars($_POST['email']);
 	$pass = htmlspecialchars($_POST['pass']);
 
 	if (preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $email)) {
 		
 		if (strlen($pass) >= 5 ) {
+			// On crypte le password avant de l'insérer en DB
+			$hash = password_hash($pass, PASSWORD_BCRYPT); 
+
 			// Inscription dans la DB
 			require_once('../model/DBConnection.php');
 
-			// On insère le nouvel utilisateur dans la DB
-			$isExist = 'SELECT email, pass FROM user WHERE email="'.$email.'" AND pass="'.$pass.'"';
+			// On vérifie si l'utilisateur n'existe pas déjà dans la DB
+			$isExist = 'SELECT email FROM user WHERE email="'.$email.'"';
 			$exist = mysqli_query($connexion, $isExist);
 			$row = mysqli_num_rows($exist);
 
 			if ($row > 0) {
 				echo "cet utilisateur existe déjà";
 			} else {
-			 	$addUser = mysqli_query($connexion,'INSERT INTO user(email, pass) VALUES ("'.$email.'", "'.$pass.'")');
+			 	$addUser = mysqli_query($connexion,'INSERT INTO user(email, pass) VALUES ("'.$email.'", "'.$hash.'")');
 			}
 		} else {
 			echo "le mot de passe doit contenir plus de 5 carractères";
 		}
 
 	} else {
-		echo "L'email est incorrect";
+		echo "Veuillez saisir un email correct";
 	}
 
 }
--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Inscription</title>
+	<title>Login</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
@@ -11,7 +11,7 @@
 <div class="container">
 
 
-<form action="Controller/SignControl.php" method="post">
+<form action="Controller/LogControl.php" method="post">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
@@ -19,10 +19,6 @@
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" name="pass" id="pass" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Confirm Password</label>
-    <input type="password" class="form-control" name="passControl" id="passControl" placeholder="Password">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
